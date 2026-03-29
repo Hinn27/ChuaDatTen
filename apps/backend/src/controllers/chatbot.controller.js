3cd73f91bdaa3e3750ecf04d5478d8e800e91d57
Hinne <duc107243@donga.edu.vn>
2026-03-29 14:17:36 +0700

    feat: update chatbot & semantic search logic

--- apps/backend/src/controllers/chatbot.controller.js
+++ apps/backend/src/controllers/chatbot.controller.js
@@ -1,11 +1,11 @@
 import * as ChatbotService from '../services/chatbot.service.js';
 
 export const askChatbot = async (req, res, next) => {
-  try {
-    const { question } = req.body;
-    const answer = await ChatbotService.chatbotAnswer(question);
-    res.json({ answer });
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const { question } = req.body;
+        const answer = await ChatbotService.chatbotAnswer(question);
+        res.json({ answer });
+    } catch (err) {
+        next(err);
+    }
 };
