e4af2a5fc88323128c3a6e243dc8c400c7493360
Hinne27 <duc107243@donga.edu.vn>
2026-04-06 20:42:22 +0700

    fix(web): bo devsupport react-buddy khoi main

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,11 +1,9 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import {BrowserRouter} from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.ts";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -17,15 +15,11 @@ const queryClient = new QueryClient({
 })
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <DevSupport ComponentPreviews={ComponentPreviews}
-                            useInitialHook={useInitial}
-                >
-                    <App/>
-                </DevSupport>
-            </BrowserRouter>
-        </QueryClientProvider>
-    </StrictMode>,
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>,
 )
